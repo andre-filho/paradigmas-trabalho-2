660863b07225051b14483f4755354e309f4677f9
Alexandre Miguel <aleronupe@gmail.com>
2019-04-26 11:14:12 -0300

    Study of like

---
+++ Aula3/gosta.pl
@@ -0,0 +1,8 @@
+gosta(maria, cafe).
+gosta(jose, cha).
+gosta(ana, guarana).
+gosta(pedro, vinho).
+gosta(jose, coca).
+gosta(maria, vinho).
+
+# Use consult('PATH_DO_ARQUIVO'). para carregar os dados
