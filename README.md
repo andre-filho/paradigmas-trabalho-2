a50a87882d0669e6e5fcfb4a8d64e1e5491e87e4
Alexandre Miguel <aleronupe@gmail.com>
2019-05-03 12:07:59 -0300

    Started project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # paradigmas-trabalho-2
+
+## Run with
+
+`$ sudo docker-compose run prolog bash`
+
+## Start prolog inside the container with
+
+`swipl`
---
+++ trabalho2.pl
@@ -0,0 +1,47 @@
+# https://en.wikipedia.org/wiki/Comparison_of_programming_languages
+# https://www.youtube.com/watch?v=2YFQlQHYXDg
+
+:-op(50, fx, '.').
+
+#language(Name,
+#         FileEx,
+#         Performance (times c lang),
+#         SintaxDificulty (1-10, 1 bad, 10 good),
+#         LearningCurve (1-10, 1 bad, 10 good),
+#         BestFor,
+#         Paradigm)
+
+language(c, '.c', 1, 4, 3, [application, sys, general purpose, low-level operations], []).
+language(c++, .cpp, 5, 3, [application, sys], []).
+language(c-sharp, .cs, , , , [application, rad, business, client-side, general, server-side, web], []).
+language(matlab, .mat, , , , , [highly_domain_specific, numerical_computing], []).
+language(scala, .scala, , , , [application, distributed, web], []).
+language(php, .php, , , , [server-side, web application, web], []).
+language(cobol, [.cbl, .cob], , , , , [application, business], []).
+language(fortran, [.f, .F, .F90, .f95, .f03], , , , [application, numerical_computing], []).
+language(kotlin, .kt, , , , [application, mobile development, server-side, client-side, web], []).
+
+language(python, .py, 2, 8, 5, [application, general, web, scripting, artificial_intelligence, scientific_computing], [aspect-oriented]).
+language(ruby, .rb, 3, 8, 5, [application, scripting, web], [aspect-oriented]).
+language(java, .java, 3, , , [application, business, client-side, general, mobile development, server-side, web], [concurrent]).
+language(go, .go, , , [application, web, server-side], [concurrent]).
+language(swift, .swift, 1, 2, 3, [application, general], [concurrent, Multi-paradigm]).
+language(prolog, .pl, , , , [application, artificial intelligence], [logic, declarative]).
+language(dart, .dart, , , , [application, web, server-side, mobile, IoT], [structured]).
+language(objective-c, [.h, .mm], , , , ,[application, general], [concurrent]).
+language(wolfram, [.nb, .cdf], , , , , [symbolic_language], [logic, distributed]).
+language(perl, .pl, , , , , [application, scripting, text processing, Web], [aspect-oriented, array, lazy evaluation, multiple dispatch, metaprogramming]).
+language(elixir, .ex, , , , [application, distributed], [concurrent, distributed]).
+language(javascript, .js,, , , , [client-side, server-side, web], [prototype-based]).
+language(assembly, .asm, , 1, 1, [General], [any]).
+language(haskell, .hs, 1.5, 6, 3, [application], [lazy_evaluation]).
+language(lua, .lua, , , , [application, scripting, embedded_scripting], [aspect-oriented]).
+
+language(birl, .birl, 1, 10, 10, [general, scripting, muscle-creation, body-building, cage-exiting, brute-force], [zueira, aspect-oriented]).      # easter egg
+# Name - Name in natural language
+# FileEx - Extension of executable file
+# Performance - Performing according to c
+# SintaxDificulty - (1-10)
+# curva de aprendizado
+# criada para (tipo: haskell -> matematica)
+
