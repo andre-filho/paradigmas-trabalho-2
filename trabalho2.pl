a127ecbdcfe9fbbfc138764be0d970f6b1b6b9a2
Andre Filho <andre.filho001@outlook.com>
2019-05-05 14:38:14 -0300

    Fixes errors and warnings, lists are no bueno

--- trabalho2.pl
+++ trabalho2.pl
@@ -1,47 +1,36 @@
-# https://en.wikipedia.org/wiki/Comparison_of_programming_languages
-# https://www.youtube.com/watch?v=2YFQlQHYXDg
-
 :-op(50, fx, '.').
 
-#language(Name,
-#         FileEx,
-#         Performance (times c lang),
-#         SintaxDificulty (1-10, 1 bad, 10 good),
-#         LearningCurve (1-10, 1 bad, 10 good),
-#         BestFor,
-#         Paradigm)
+language(c, '.c', 1, 4, 3, [application, sys, general_purpose, low_level_operations], [multiple]).
+language(c_plus_plus, '.cpp', 0, 5, 3, [application, sys], [multiple]).
+language(c_sharp, '.cs', 3, 4, 4, [application, rad, business, client_side, general, server_side, web], [multiple]).
+language(matlab, '.mat', 0.5, 4, 5, [highly_domain_specific, numerical_computing], [multiple]).
+language(scala, '.scala', 0, 5, 6, [application, distributed, web], [multiple]).
+language(php, '.php', 0, 7, 7, [server_side, web_application, web], [multiple]).
+language(cobol, '.cbl', 0, 2, 3, [application, business], [multiple]).
+language(fortran, '.f', 0, 4, 4, [application, numerical_computing], [multiple]).
+language(kotlin, '.kt', 0, 5, 5, [application, mobile_development, server_side, client_side, web], [multiple]).
+language(python, '.py', 2, 8, 5, [application, general, web, scripting, artificial_intelligence, scientific_computing], [aspect_oriented]).
+language(ruby, '.rb', 3, 8, 5, [application, scripting, web], [aspect_oriented]).
+language(java, '.java', 3, 4, 4, [application, business, client_side, general, mobile_development, server_side, web], [concurrent]).
+language(go, '.go', 0, 7, 5, [application, web, server_side], [concurrent]).
+language(swift, '.swift', 1, 2, 3, [application, general], [concurrent, multi_paradigm]).
+language(prolog, '.pl', 0, 7, 5, [application, artificial_intelligence], [logic, declarative]).
+language(dart, '.dart', 0, 5, 5, [application, web, server_side, mobile, iot], [structured]).
+language(objective_c, '.h', 0, 5, 5, [application, general], [concurrent]).
+language(wolfram, '.nb', 0, 2, 6, [symbolic_language], [logic, distributed]).
+language(perl, '.pl', 0, 7, 3, [application, scripting, text_processing, web], [aspect_oriented, array, lazy_evaluation, multiple_dispatch, metaprogramming]).
+language(elixir, '.ex', 0, 5, 5, [application, distributed], [concurrent, distributed]).
+language(javascript, '.js', 0, 6, 6, [client_side, server_side, web], [prototype_based]).
+language(assembly, '.asm', 0, 1, 1, [general], [any]).
+language(haskell, '.hs', 1.5, 6, 3, [application], [lazy_evaluation]).
+language(lua, '.lua', 0, 2, 5, [application, scripting, embedded_scripting], [aspect_oriented]).
+language(birl, '.birl', 1, 10, 10, [general, scripting, muscle_creation, body_building, cage_exiting, brute_force], [zueira, aspect_oriented]).
 
-language(c, '.c', 1, 4, 3, [application, sys, general purpose, low-level operations], []).
-language(c++, .cpp, 5, 3, [application, sys], []).
-language(c-sharp, .cs, , , , [application, rad, business, client-side, general, server-side, web], []).
-language(matlab, .mat, , , , , [highly_domain_specific, numerical_computing], []).
-language(scala, .scala, , , , [application, distributed, web], []).
-language(php, .php, , , , [server-side, web application, web], []).
-language(cobol, [.cbl, .cob], , , , , [application, business], []).
-language(fortran, [.f, .F, .F90, .f95, .f03], , , , [application, numerical_computing], []).
-language(kotlin, .kt, , , , [application, mobile development, server-side, client-side, web], []).
 
-language(python, .py, 2, 8, 5, [application, general, web, scripting, artificial_intelligence, scientific_computing], [aspect-oriented]).
-language(ruby, .rb, 3, 8, 5, [application, scripting, web], [aspect-oriented]).
-language(java, .java, 3, , , [application, business, client-side, general, mobile development, server-side, web], [concurrent]).
-language(go, .go, , , [application, web, server-side], [concurrent]).
-language(swift, .swift, 1, 2, 3, [application, general], [concurrent, Multi-paradigm]).
-language(prolog, .pl, , , , [application, artificial intelligence], [logic, declarative]).
-language(dart, .dart, , , , [application, web, server-side, mobile, IoT], [structured]).
-language(objective-c, [.h, .mm], , , , ,[application, general], [concurrent]).
-language(wolfram, [.nb, .cdf], , , , , [symbolic_language], [logic, distributed]).
-language(perl, .pl, , , , , [application, scripting, text processing, Web], [aspect-oriented, array, lazy evaluation, multiple dispatch, metaprogramming]).
-language(elixir, .ex, , , , [application, distributed], [concurrent, distributed]).
-language(javascript, .js,, , , , [client-side, server-side, web], [prototype-based]).
-language(assembly, .asm, , 1, 1, [General], [any]).
-language(haskell, .hs, 1.5, 6, 3, [application], [lazy_evaluation]).
-language(lua, .lua, , , , [application, scripting, embedded_scripting], [aspect-oriented]).
+bestperformance().
 
-language(birl, .birl, 1, 10, 10, [general, scripting, muscle-creation, body-building, cage-exiting, brute-force], [zueira, aspect-oriented]).      # easter egg
-# Name - Name in natural language
-# FileEx - Extension of executable file
-# Performance - Performing according to c
-# SintaxDificulty - (1-10)
-# curva de aprendizado
-# criada para (tipo: haskell -> matematica)
+worstperformance().
 
+goodforwebdevelopment().
+
+multipleparadigms(language(Name, Extension, Performance, SyntaxEase, LearningCurve, GoodFor, [multiple])).
